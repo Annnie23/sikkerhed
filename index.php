09ea4390c6e488bd3400506ddfdf78fa4e8777f3
Annnie23 <143446906+Annnie23@users.noreply.github.com>
2025-02-07 10:02:09 +0100

    up

--- index.php
+++ index.php
@@ -29,10 +29,11 @@ get_header(); // Indlæser headeren
         <div class="overlay">
             <div class="content-container">
                 <div class="left-side">
+                    <h2><?php echo esc_html(get_field('new_section_header')); ?></h2>
                     <p><?php echo esc_html(get_field('new_section_description')); ?></p>
                 </div>
                 <div class="right-side">
-                    <h2><?php echo esc_html(get_field('new_section_header')); ?></h2>
+                    <h2><?php echo esc_html(get_field('new_section_header2')); ?></h2>
                     <select class="chapter-dropdown" onchange="window.location.href=this.value;">
                         <option value="<?php echo get_permalink(186); ?>#kapitel-1">Kapitel 1</option>
                         <option value="<?php echo get_permalink(186); ?>#kapitel-2">Kapitel 2</option>
@@ -113,7 +114,7 @@ get_header(); // Indlæser headeren
 <div class="link-section-2">
         <h2><?php echo esc_html(get_field('test_section_header')); ?></h2>
         <p><?php echo esc_html(get_field('test_section_text')); ?></p>
-        <a href="http://sikkerhedunderskydning.local/quiz/" class="button" id="link_knap_1">Tag testen om skydesikkerhed her</a>
+        <a href="http://sikkerhedunderskydning.local/quiz/" class="button2" id="link_knap_2">Tag testen om skydesikkerhed her</a>
 </div>
 
 
